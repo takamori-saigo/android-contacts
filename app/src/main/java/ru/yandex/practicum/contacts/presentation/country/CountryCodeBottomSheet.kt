4e9b19c8f677403f32bb1288a127d3a94129d268
takamori-saigo <anamneshev1999@gmail.com>
2026-03-11 12:32:14 +0500

    Refactor: replace CountryCode and Messengers bottom sheets with generic CommonBottomSheet

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/country/CountryCodeBottomSheet.kt
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/country/CountryCodeBottomSheet.kt
@@ -1,25 +1,7 @@
 package ru.yandex.practicum.contacts.presentation.country
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.BottomSheetDefaults
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,78 +9,27 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import ru.yandex.practicum.contacts.R
 import ru.yandex.practicum.contacts.data.models.CountryCode
+import ru.yandex.practicum.contacts.presentation.ui.components.CommonBottomSheet
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CountryCodeBottomSheet(
     selectedCodes: Set<CountryCode>,
     onCodesSelected: (Set<CountryCode>) -> Unit,
     onDismiss: () -> Unit
 ) {
-    ModalBottomSheet(
-        onDismissRequest = onDismiss,
-        dragHandle = { BottomSheetDefaults.DragHandle() }
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = stringResource(R.string.filter_by_country_code),
-                    style = MaterialTheme.typography.titleLarge
-                )
-                IconButton(onClick = onDismiss) {
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        contentDescription = stringResource(R.string.close)
-                    )
-                }
-            }
-
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            ) {
-                items(CountryCode.COMMON_CODES) { countryCode ->
-                    val isSelected = selectedCodes.contains(countryCode)
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 4.dp),
-                        onClick = {
-                            val newSelection = selectedCodes.toMutableSet()
-                            if (isSelected) {
-                                newSelection.remove(countryCode)
-                            } else {
-                                newSelection.add(countryCode)
-                            }
-                            onCodesSelected(newSelection)
-                        },
-                        color = if (isSelected) {
-                            MaterialTheme.colorScheme.primaryContainer
-                        } else {
-                            MaterialTheme.colorScheme.surface
-                        }
-                    ) {
-                        CountryCodeOption(
-                            isSelected = isSelected,
-                            countryCode = countryCode,
-                            selectedCodes = selectedCodes,
-                            onCodesSelected = onCodesSelected
-                        )
-                    }
-                }
-            }
-        }
+    CommonBottomSheet(
+        title = stringResource(R.string.filter_by_country_code),
+        items = CountryCode.COMMON_CODES,
+        selectedItems = selectedCodes,
+        onItemsSelected = onCodesSelected,
+        onDismiss = onDismiss
+    ) { countryCode, isSelected ->
+        CountryCodeOption(
+            isSelected = isSelected,
+            countryCode = countryCode,
+            selectedCodes = selectedCodes,
+            onCodesSelected = onCodesSelected
+        )
     }
 }
 
--- app/src/main/java/ru/yandex/practicum/contacts/presentation/messengers/MessengersBottomSheet.kt
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/messengers/MessengersBottomSheet.kt
@@ -1,25 +1,7 @@
 package ru.yandex.practicum.contacts.presentation.messengers
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.BottomSheetDefaults
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,78 +9,27 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import ru.yandex.practicum.contacts.R
 import ru.yandex.practicum.contacts.data.models.MessagingApp
+import ru.yandex.practicum.contacts.presentation.ui.components.CommonBottomSheet
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MessengersBottomSheet(
     selectedApps: Set<MessagingApp>,
     onAppsSelected: (Set<MessagingApp>) -> Unit,
     onDismiss: () -> Unit
 ) {
-    ModalBottomSheet(
-        onDismissRequest = onDismiss,
-        dragHandle = { BottomSheetDefaults.DragHandle() }
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = stringResource(R.string.filter_by_messaging_app),
-                    style = MaterialTheme.typography.titleLarge
-                )
-                IconButton(onClick = onDismiss) {
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        contentDescription = stringResource(R.string.close)
-                    )
-                }
-            }
-
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            ) {
-                items(MessagingApp.entries) { app ->
-                    val isSelected = selectedApps.contains(app)
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 4.dp),
-                        onClick = {
-                            val newSelection = selectedApps.toMutableSet()
-                            if (isSelected) {
-                                newSelection.remove(app)
-                            } else {
-                                newSelection.add(app)
-                            }
-                            onAppsSelected(newSelection)
-                        },
-                        color = if (isSelected) {
-                            MaterialTheme.colorScheme.primaryContainer
-                        } else {
-                            MaterialTheme.colorScheme.surface
-                        }
-                    ) {
-                        MessengerOption(
-                            isSelected = isSelected,
-                            app = app,
-                            selectedApps = selectedApps,
-                            onAppsSelected = onAppsSelected
-                        )
-                    }
-                }
-            }
-        }
+    CommonBottomSheet(
+        title = stringResource(R.string.filter_by_messaging_app),
+        items = MessagingApp.entries,
+        selectedItems = selectedApps,
+        onItemsSelected = onAppsSelected,
+        onDismiss = onDismiss
+    ) { app, isSelected ->
+        MessengerOption(
+            isSelected = isSelected,
+            app = app,
+            selectedApps = selectedApps,
+            onAppsSelected = onAppsSelected
+        )
     }
 }
 
